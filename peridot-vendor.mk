f496edff73e997704103097c244f5c75b7950717
NotBlazeee28 <p.devansh@proton.me>
2026-01-11 08:50:21 +0000

    peridot: Use libprotobuf-cpp-{lite,full}-21.7 from hardware/lineage/compat
    Change-Id: I07eb8e11057f254bfdaaf3af128d526c9d127ae1

--- peridot-vendor.mk
+++ peridot-vendor.mk
@@ -1429,8 +1429,6 @@ PRODUCT_PACKAGES += \
     libperfioctl \
     libperipheral_client \
     libpolygon \
-    libprotobuf-cpp-full-21.7 \
-    libprotobuf-cpp-lite-21.7 \
     libq-perflog \
     libqc2audio_base \
     libqc2audio_basecodec \
